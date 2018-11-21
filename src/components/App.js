2564ca65979b97f7f551fb46ab8cac1c0dc924b3
K SUH <kangone@grana.com>
2018-11-22 02:57:32 +0800

    create-upload: Create working version of uploader component

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,29 @@ import ColumnSelector from './ColumnSelector';
 import Map from './Map';
 
 class App extends React.Component {
-    
+    state = {
+        currentTable: [],
+        tables: [],
+    };
+
+    setCurrentTable = (data) => {
+        let currentTable = this.state.currentTable;
+        currentTable = data;
+        this.setState({ currentTable });
+    };
+
+    addTable = (data) => {
+        let tables = this.state.tables;
+        tables.push(data);
+        this.setState({ tables });
+    }
+
     render() {
         return (
             <div className="container">
-                <Uploader />
-                <ColumnSelector />
-                <Map />
+                <Uploader setCurrentTable={this.setCurrentTable} />
+                <ColumnSelector currentTable={this.state.currentTable} addTable={this.addTable} />
+                <Map tables={this.state.tables} />
             </div>
         );
     }
--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import CSVReader from "react-csv-reader";
 
 class Uploader extends React.Component {
+
+    state = {
+        validationMsg: '',
+    };
+
+    onFileLoaded = (data) => {
+        console.log(data);
+        if (this.verifyFormat(data)) {
+            this.props.setCurrentTable(data);
+        }
+    }
+
+    setValidationMsg = (s) => {
+        let validationMsg = this.state.validationMsg;
+        validationMsg = s;
+        this.setState({ validationMsg })
+    }
+
+    verifyFormat = data => {
+        const len = data.length;
+        if (!len) {
+            // file is empty
+            this.setValidationMsg('File is empty!');
+            return false;
+        }
+
+        for (let i = 0; i < len; i++) {
+            if (data[i].length !== 5) {
+                const msg = `${i+1}th row does not have 5 columns!`;
+                this.setValidationMsg(msg);
+                return false;
+            }
+        }
+
+        return true;
+    }
     
     render() {
         return (
-            <h1>uploader</h1>
+            <div className="container">
+                <h1>Upload</h1>
+                <CSVReader onFileLoaded={this.onFileLoaded} />
+                <h4 className="text-danger">{this.state.validationMsg}</h4>
+          </div>
         );
     }
 }
