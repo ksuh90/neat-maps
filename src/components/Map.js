f72466bd00f71fd87da080399d20106659cec370
K SUH <kangone@grana.com>
2018-11-22 05:40:09 +0800

    create-normalize: Add bottm margin

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,7 @@ class Map extends React.Component {
     
     render() {
         return (
-            <div className="container border mt-3">
+            <div className="container border mt-3 mb-5">
                 <h1>Map</h1>
             </div>
         );
