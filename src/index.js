cd5121b2214c84432d4bcb0fbbfce341a87e80ee
K SUH <kangone@grana.com>
2018-11-23 09:07:05 +0800

    master: Remove router component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Router from './components/Router';
+import App from './components/App';
 
-render(<Router />, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
